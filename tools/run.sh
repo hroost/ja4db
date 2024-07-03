9800c8f5d75c3b3543ec2afa2dd803db4a6c184c
Aleksei <amozgovenko@finst.com>
2024-07-03 13:26:56 +0200

    add array sorting

--- tools/run.sh
+++ tools/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl -sL "https://ja4db.com/api/download/" | jq . -S > ja4db.json && \
+curl -sL --fail "https://ja4db.com/api/download/" | jq '. | sort' -S > ja4db.json && \
   jq '. | length' ja4db.json
