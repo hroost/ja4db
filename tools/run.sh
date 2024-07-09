77b3a1d2d9476cac443dcb89fad134dfdb3fd6c6
Aleksei <amozgovenko@finst.com>
2024-07-09 23:56:43 +0200

    fix

--- tools/run.sh
+++ tools/run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+set -x
 
-curl -sL --fail "https://ja4db.com/api/download/" | jq '. | sort' -S > ja4db.json && \
+curl -sL -k --fail "https://ja4db.com/api/download/" | jq '. | sort' -S > ja4db.json && \
   jq '. | length' ja4db.json
 
-jq '.[] | select(.verified == true)' ja4db.json > ja4db_verified.json && \
+jq '[.[] | select(.verified == true)]' ja4db.json > ja4db_verified.json && \
   jq '. | length' ja4db_verified.json
