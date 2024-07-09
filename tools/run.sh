2a390fd3af23d6d16918f4faf0e45ad8e36e4dc8
Aleksei <amozgovenko@finst.com>
2024-07-09 23:34:51 +0200

    verified flag

--- tools/run.sh
+++ tools/run.sh
@@ -2,3 +2,6 @@
 
 curl -sL --fail "https://ja4db.com/api/download/" | jq '. | sort' -S > ja4db.json && \
   jq '. | length' ja4db.json
+
+jq '.[] | select(.verified == true)' ja4db.json > ja4db_verified.json && \
+  jq '. | length' ja4db_verified.json
