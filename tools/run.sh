a11e7637fb8c2e1c5f385d84ebfe64f3d3e4406c
Aleksei <amozgovenko@finst.com>
2024-07-13 00:16:45 +0200

    output data in git friendly style

--- tools/run.sh
+++ tools/run.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 set -x
 
-curl -sL -k --fail "https://ja4db.com/api/download/" | jq '. | sort' -S > ja4db.json && \
-  jq '. | length' ja4db.json
+curl -sL -k --fail "https://ja4db.com/api/download/" | jq '. | sort' -S > db.json && \
+  jq '. | length' db.json
 
-jq '[.[] | select(.verified == true)]' ja4db.json > ja4db_verified.json && \
-  jq '. | length' ja4db_verified.json
+# Array with each element on a new line
+cat db.json | jq -c '.' | sed 's/{"application/\n{"application/g' | sed 's/}]/}\n]/g' > ja4db.json && \
+jq '. | length' ja4db.json
+
+# Filtered verified hashes
+cat db.json | jq -c '[.[] | select(.verified == true)]' | sed 's/{"application/\n{"application/g' | sed 's/}]/}\n]/g' > ja4db_verified.json && \
+jq '. | length' ja4db_verified.json
