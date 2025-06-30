1eabafccf2600d9768da6c4a8b9a247259318254
Aleksei <amozgovenko@finst.com>
2025-06-30 09:39:08 +0200

    to prevent file sizes go over 100MB lets strip empty values

--- tools/run.sh
+++ tools/run.sh
@@ -5,9 +5,9 @@ curl -sL -k --fail "https://ja4db.com/api/download/" | jq '. | sort' -S > db.jso
   jq '. | length' db.json
 
 # Array with each element on a new line
-cat db.json | jq -c '.' | sed 's/{"application/\n{"application/g' | sed 's/}]/}\n]/g' > ja4db.json && \
+jq -c 'walk(if type == "object" then with_entries(select(.value != null and .value != "")) else . end)' db.json | sed 's/{/\n{/g' | sed 's/}]/}\n]/g' > ja4db.json && \
 jq '. | length' ja4db.json
 
 # Filtered verified hashes
-cat db.json | jq -c '[.[] | select(.verified == true)]' | sed 's/{"application/\n{"application/g' | sed 's/}]/}\n]/g' > ja4db_verified.json && \
+jq -c '[.[] | select(.verified == true) | walk(if type == "object" then with_entries(select(.value != null and .value != "")) else . end)]' db.json | sed 's/{/\n{/g' | sed 's/}]/}\n]/g' > ja4db_verified.json && \
 jq '. | length' ja4db_verified.json
